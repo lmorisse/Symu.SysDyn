d63cc65e9cef6005d61570c7ccb3205a59ba2c50
clementdepond <clement.depond53@gmail.com>
2021-02-24 10:28:36 +0100

    ComplexEquation changes

--- SourceCode/SymuSysDynTests/Equations/ComplexEquationTests.cs
+++ SourceCode/SymuSysDynTests/Equations/ComplexEquationTests.cs
@@ -114,12 +114,6 @@ namespace SymuSysDynTests.Equations
             Assert.AreEqual(5, variable.Equation.Evaluate(null, Variables, null));
         }
 
-                public void EvaluateTest10()
-        {
-            var variable = Variable.CreateInstance("X", Model, NestedEquation);
-            Assert.AreEqual(5, variable.Equation.Evaluate(null, Variables, null));
-        }
-
         #endregion
 
         #region Replace
