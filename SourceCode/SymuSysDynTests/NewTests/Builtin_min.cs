e0fc8ee3ef3eae1b4fdcc225541b0e537fc30b43
KessalR <rabeaa.kessal@gmail.com>
2021-03-19 11:11:40 +0100

    Builtin_min test

---
+++ SourceCode/SymuSysDynTests/NewTests/Builtin_min.cs
@@ -0,0 +1,78 @@
+﻿#region Licence
+
+// Description: SymuSysDyn - SymuSysDynTests
+// Website: https://symu.org
+// Copyright: (c) 2021 laurent Morisseau
+// License : the program is distributed under the terms of the GNU General Public License
+
+#endregion
+
+#region using directives
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Symu.SysDyn.Core.Engine;
+using Symu.SysDyn.Core.Models.XMile;
+using Symu.SysDyn.Tests.Classes;
+#endregion
+
+namespace Symu.SysDyn.Tests.NewTests
+{
+    [TestClass]
+    public class Builtin_min : FunctionClassTest
+    {
+        protected const string TestFile = ClassPath.classpath + "Builtin_min.xmile";
+
+        /// <summary>
+        ///     It is not the Machine.Variables
+        /// </summary>
+        protected XMileModel Variables { get; } = new XMileModel("1");
+
+        protected XDocument XDoc { get; private set; }
+        protected XNamespace Ns { get; private set; }
+        protected XElement XElement { get; set; }
+        protected StateMachine Machine { get; private set; }
+
+        [TestInitialize]
+        public async Task InitializeTest()
+        {
+            Machine = await StateMachine.CreateStateMachine(TestFile);
+            XDoc = XDocument.Load(TestFile);
+            Ns = XDoc.Root?.Attributes("xmlns").First().Value;
+            XElement = XDoc.Root?.Descendants(Ns + "variables").First();
+        }
+
+        [TestMethod]
+        public void StateMachineTest()
+        {
+            Assert.IsNotNull(Machine.Simulation);
+            Assert.IsNotNull(Machine.Results);
+            Assert.IsNotNull(Machine.ReferenceVariables);
+            Assert.IsNotNull(Machine.Variables);
+            Assert.AreEqual(5, Machine.Variables.Count());
+        }
+
+        /// <summary>
+        /// Check if the variable is the min
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task OptimizeTest()
+        {
+            Machine.Optimized = false;
+            await Machine.Prepare();
+            Assert.AreEqual(5, Machine.Variables.Count());
+            Assert.IsNotNull(Machine.Variables);
+            var minValue = Machine.Variables.Get("_Output");
+            Assert.IsNotNull(minValue);
+            Assert.AreEqual(0, minValue.Value);
+            Machine.Process();
+            var variable = Machine.Variables.Get("_Output");
+            Assert.IsNotNull(variable);
+            Assert.AreNotEqual(minValue, variable.Value);
+
+        }
+    }
+
+}
