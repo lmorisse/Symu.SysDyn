b7a010474c538ffb4911d83a5e0b68090d9e1a6c
Varieras Leo <leo.varieras@etudiant.univ-rennes1.fr>
2021-03-19 14:53:05 +0100

    Test Reference Capitalization

---
+++ SourceCode/SymuSysDynTests/NewTests/ReferenceCapitalization.cs
@@ -0,0 +1,59 @@
+﻿#region Licence
+
+// Description: SymuSysDyn - SymuSysDynTests
+// Website: https://symu.org
+// Copyright: (c) 2021 laurent Morisseau
+// License : the program is distributed under the terms of the GNU General Public License
+
+#endregion
+
+#region using directives
+
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Symu.SysDyn.Tests.Classes;
+using System.Xml.Linq;
+using Symu.SysDyn.Core.Engine;
+using Symu.SysDyn.Core.Models.XMile;
+
+#endregion
+
+namespace Symu.SysDyn.Tests.NewTests
+{
+    [TestClass]
+    public class ReferenceCapitalization : FunctionClassTest
+    {
+        
+        [TestInitialize]
+        public async Task InitializeTest()
+        {
+            await Initialize("ReferenceCapitalization.xmile");
+        }
+
+        [TestMethod]
+        public override void StateMachineTest()
+        {
+            base.StateMachineTest();
+            Assert.AreEqual(8, Machine.Variables.Count());
+        }
+
+        /// <summary>
+        /// Check if non-dynamic variable consider capitalization on the xmile file and test the abs(int) function
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task OptimizeTest()
+        {
+            Machine.Optimized = false;
+            await Machine.Prepare();
+            Assert.IsNotNull(Machine.Variables);
+            var variable = Machine.Variables.Get("_Test1");
+            Assert.IsNotNull(variable);
+            Assert.AreEqual(5, variable.Value);
+            variable = Machine.Variables.Get("_Test2");
+            Assert.IsNotNull(variable);
+            Assert.AreEqual(5, variable.Value);
+        }
+    }
+}
