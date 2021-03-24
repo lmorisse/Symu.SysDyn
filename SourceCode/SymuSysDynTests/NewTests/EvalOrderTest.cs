a6c0101ffd5d83050ae48fe981415594cfa17ab6
clementdepond <clement.depond53@gmail.com>
2021-03-24 17:23:47 +0100

    Eval order test

---
+++ SourceCode/SymuSysDynTests/NewTests/EvalOrderTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Symu.SysDyn.Tests.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Symu.SysDyn.Tests.NewTests
+{
+    [TestClass]
+    public class EvalOrderTest : FunctionClassTest
+    {
+        [TestInitialize]
+        public async Task InitializeTest()
+        {
+            await Initialize("Eval_order.xmile");
+        }
+
+        [TestMethod]
+        public override void StateMachineTest()
+        {
+            base.StateMachineTest();
+            Assert.AreEqual(1, Machine.Variables.Count());
+        }
+
+        /// <summary>
+        /// Check if application execute equation variable in the good order
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task OptimizeTest()
+        {
+            Machine.Optimized = false;
+            await Machine.Prepare();
+            Assert.AreEqual(1, Machine.Variables.Count());
+            Assert.IsNotNull(Machine.Variables);
+            var variable = Machine.Variables.Get("_Auxiliary");
+            Assert.IsNotNull(variable);
+            Assert.AreEqual(5, variable.Value);
+        }
+    }
+}
