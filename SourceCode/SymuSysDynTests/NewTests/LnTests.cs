99fa456f0c7c75266ed0470209bf1ba1d7e21d65
clementdepond <clement.depond53@gmail.com>
2021-03-26 16:20:57 +0100

    LnTests

---
+++ SourceCode/SymuSysDynTests/NewTests/LnTests.cs
@@ -0,0 +1,57 @@
+﻿#region Licence
+
+// Description: SymuSysDyn - SymuSysDynTests
+// Website: https://symu.org
+// Copyright: (c) 2021 laurent Morisseau
+// License : the program is distributed under the terms of the GNU General Public License
+
+#endregion
+
+#region using directives
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Symu.SysDyn.Tests.Classes;
+#endregion
+
+namespace Symu.SysDyn.Tests.NewTests
+{
+    [TestClass]
+    public class LnTests : FunctionClassTest
+    {
+        [TestInitialize]
+        public async Task InitializeTest()
+        {
+            await Initialize("Ln.xmile");
+        }
+
+        [TestMethod]
+        public override void StateMachineTest()
+        {
+            base.StateMachineTest();
+            Assert.AreEqual(7, Machine.Variables.Count());
+        }
+
+        /// <summary>
+        /// Check if the variable is the max
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task OptimizeTest()
+        {
+            Machine.Optimized = false;
+            await Machine.Prepare();
+            Assert.AreEqual(7, Machine.Variables.Count());
+            Assert.IsNotNull(Machine.Variables);
+            var variable = Machine.Variables.Get("_Test_ln");
+            Assert.IsNotNull(variable);
+            Assert.AreEqual(-9.21F, (float)Math.Round(variable.Value, 2));
+            Machine.Process();
+            variable = Machine.Variables.Get("_Test_ln");
+            Assert.IsNotNull(variable);
+            Assert.AreEqual(4.65F, (float)Math.Round(variable.Value, 2));
+        }
+    }
+}
