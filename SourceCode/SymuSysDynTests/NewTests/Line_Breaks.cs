b19965d7a6cb1b923393ffe8e0f9d695f68255d2
Ehuar <rebeccanellyehua@gmail.com>
2021-03-26 09:29:25 +0100

    Line_Break tests

---
+++ SourceCode/SymuSysDynTests/NewTests/Line_Breaks.cs
@@ -0,0 +1,56 @@
+﻿#region Licence
+
+// Description: SymuSysDyn - SymuSysDynTests
+// Website: https://symu.org
+// Copyright: (c) 2021 laurent Morisseau
+// License : the program is distributed under the terms of the GNU General Public License
+
+#endregion
+
+#region using directives
+
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Symu.SysDyn.Tests.Classes;
+
+#endregion
+
+namespace Symu.SysDyn.Tests.Simulation
+{
+    [TestClass]
+    public class LineBreaks : FunctionClassTest
+    {
+        [TestInitialize]
+        public async Task InitializeTest()
+        {
+            await Initialize("Line_Breaks.xmile");
+        }
+
+        [TestMethod]
+        public override void StateMachineTest()
+        {
+            base.StateMachineTest();
+            Assert.AreEqual(7, Machine.Variables.Count());
+        }
+
+        /// <summary>
+        /// tests the ability of an analyzer to manage a line break in the description of a variable
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task OptimizeTest()
+        {
+            Machine.Optimized = false;
+            await Machine.Prepare();
+            Assert.AreEqual(7, Machine.Variables.Count());
+            Assert.IsNotNull(Machine.Variables);
+            var variable = Machine.Variables.Get("_Downstream");
+            Assert.IsNotNull(variable);
+            Assert.AreEqual(7, variable.Value);
+            Machine.Process();
+            Assert.AreEqual(7, variable.Value);
+
+        }
+    }
+}
